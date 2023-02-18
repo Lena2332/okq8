988d9c78c258c9f429fceffd1506aaec3931acd3
olekup <olena.kupriiets@consid.se>
2023-02-18 13:10:32 +0100

    Add foreign key to stations table.

---
+++ database/migrations/2023_02_18_115316_change_stations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stations', function (Blueprint $table) {
+            $table->bigInteger('destination_id')->unsigned();
+            $table->foreign('destination_id')->references('id')->on('destinations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stations', function (Blueprint $table) {
+            $table->dropForeign(['destination_id']);
+            $table->dropColumn('destination_id');
+        });
+    }
+};
