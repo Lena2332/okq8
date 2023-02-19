b2d3d612a0038cbc67cb87cf426bee1e35319b85
olekup <olena.kupriiets@consid.se>
2023-02-19 10:48:39 +0100

    Add configuration file for experiences.
    Add possibility to add icons.

---
+++ config/experience.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'experience' => [
+        'movie' => 'icon1',
+        'children_room' => 'icon2',
+        'restaurant' => 'icon3',
+        'hotel' => 'icon4'
+    ],
+
+    'possibilities' => [
+        'toilet' => 'icon6',
+        'eat' => 'icon7',
+        'children' => 'icon5',
+    ]
+];
